148eb2960b0c547cc82d8b4b350eac0e553a809a
Kayque Goldner <kayquegoldner@gmail.com>
2025-07-07 18:48:17 -0300

    add home page

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
@@ -8,5 +9,6 @@ import Header from "../components/Header.vue";
   >
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,14 @@
-<template>
-  <div>
-    <h1>Home</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import SearchInput from "../components/SearchInput.vue";
+</script>
 
-<script setup lang="ts"></script>
+<template>
+  <main class="flex-1 size-full flex flex-col items-center pt-24 gap-10">
+    <h1 class="text-2xl sm:text-4xl font-semibold tracking-wider text-center">
+      PESQUISAR CIDADE
+    </h1>
+    <div class="w-full max-w-2xl">
+      <SearchInput />
+    </div>
+  </main>
+</template>
